029ebf25e91c9c36283a9edc974e7bec0ee16b9c
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-22 07:23:06 +0530

    handle setup

--- src/layouts/Setup.tsx
+++ src/layouts/Setup.tsx
@@ -4,13 +4,14 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 const Setup = () => {
   const { user } = useAppSelector((state) => state.auth);
   const location = useLocation();
-  console.log("object", user);
+  console.log(user);
 
   if (user === null) {
-    return (
-      <Navigate to={`/auth/login?redirect=${location.pathname}`} replace />
-    );
-  } else if (user.storeId !== null) {
+    const redirect =
+      new URLSearchParams(location.search).get("redirect") || "/";
+
+    return <Navigate to={redirect} replace />;
+  } else if (user.storeId.length > 0) {
     return <Navigate to={`/${user.storeId[0]}`} replace />;
   }
 
