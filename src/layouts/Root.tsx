0a86ec891fb3719cec67765ac0f2c2f6d3fa7763
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-23 14:50:57 +0530

    add components

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,10 +1,12 @@
 import ModalProvider from "@/providers/ModalProvider";
+import SheetProvider from "@/providers/SheetProvider";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
     <>
       <ModalProvider />
+      <SheetProvider />
       <Outlet />
     </>
   );
