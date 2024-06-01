dc54838cc13b010ef5a432e8aa0f4654ee8ee91a
sandeep7567 <sandeepthakur7567@gmail.com>
2024-06-01 20:58:47 +0530

    add price_conversion value

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,8 @@
 export const CURRENT_PAGE = 1;
 export const PER_PAGE = 6;
 export const DEBOUNCE_TIMER = 500;
+export const PRICE_CONVERSION = 100;
+
 export const ROLES = {
   ADMIN: "admin",
   CUSTOMER: "customer",
